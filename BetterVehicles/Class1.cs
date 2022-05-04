e441622b9b72b31d73f9b22ba4e7e98bf189f746
dt22 <dtony22@gmail.com>
2022-05-04 00:13:30 -0500

    Fixed Regen and Enrage from linus mod

--- BetterVehicles/Class1.cs
+++ BetterVehicles/Class1.cs
@@ -16,6 +16,7 @@ using PhoenixPoint.Common.Entities.Characters;
 using PhoenixPoint.Common.Entities.Equipments;
 using PhoenixPoint.Common.Entities.GameTags;
 using PhoenixPoint.Common.Entities.GameTagsTypes;
+using PhoenixPoint.Common.Entities.Items;
 using PhoenixPoint.Common.UI;
 using PhoenixPoint.Geoscape.Entities.DifficultySystem;
 using PhoenixPoint.Geoscape.Entities.Research;
@@ -194,7 +195,52 @@ namespace BetterVehicles
             mutogVenomHead.DamagePayload.DamageKeywords[1].Value = 80;
             //mutogVenomHead.UseAimIK = true;
             BodyPartAspectDef MAL = (BodyPartAspectDef)mutogAgileLegs.BodyPartAspectDef;
-            MAL.Speed = 11;      
+            MAL.Speed = 11;
+
+            FrenzyStatusDef frenzyStatusDef = Repo.GetAllDefs<FrenzyStatusDef>().FirstOrDefault((FrenzyStatusDef w) => w.name == "Mutog_Enraged_StatusDef");
+            frenzyStatusDef.SpeedCoefficient = 1f;
+            HealthConditionStatusDef healthConditionStatusDef = Repo.GetAllDefs<HealthConditionStatusDef>().FirstOrDefault((HealthConditionStatusDef w) => w.name == "Mutog_HealthCondition_StatusDef");
+            healthConditionStatusDef.Thresholds[0].HealthRatioThreshold = 0.3f;
+
+            int usesPerTurn = 1;
+            float actionPointCost = 0.5f;
+            int num = 3;
+            int num2 = 10;
+            bool targetFirstDamagedBodypart = false;
+            foreach (ItemDef itemDef in from ad in Repo.GetAllDefs<ItemDef>()
+                                        where ad.name.Contains("Leg_Regenerating") && ad.name.Contains("BodyPartDef")
+                                        select ad)
+            {
+                itemDef.Armor = 25f;
+                itemDef.HitPoints = 160f;
+            }
+            TacticalActorDef tacticalActorDef = Repo.GetAllDefs<TacticalActorDef>().FirstOrDefault((TacticalActorDef ta) => ta.name.Equals("Mutog_ActorDef"));
+            ApplyStatusAbilityDef item = Repo.GetAllDefs<ApplyStatusAbilityDef>().FirstOrDefault((ApplyStatusAbilityDef ad) => ad.name.Equals("Unbreakable_AbilityDef"));
+            ApplyStatusAbilityDef applyStatusAbilityDef = Repo.GetAllDefs<ApplyStatusAbilityDef>().FirstOrDefault((ApplyStatusAbilityDef w) => w.name == "Mutog_Regeneration_AbilityDef");
+            AbilityDef item2 = Repo.GetAllDefs<AbilityDef>().FirstOrDefault((AbilityDef w) => w.name == "GroundStomp_AbilityDef");
+            AbilityDef item3 = Repo.GetAllDefs<AbilityDef>().FirstOrDefault((AbilityDef w) => w.name == "Mutog_Ram_AbilityDef");
+            RamAbilityDef ramAbilityDef = Repo.GetAllDefs<RamAbilityDef>().FirstOrDefault((RamAbilityDef w) => w.name == "Mutog_Ram_AbilityDef");
+            ItemDef itemDef2 = Repo.GetAllDefs<ItemDef>().FirstOrDefault((ItemDef w) => w.name.Contains("Mutog_RearRightLeg_Regenerating_BodyPartDef"));
+            GooDamageMultiplierAbilityDef item4 = Repo.GetAllDefs<GooDamageMultiplierAbilityDef>().FirstOrDefault((GooDamageMultiplierAbilityDef w) => w.name == "GooImmunity_AbilityDef");
+            DamageMultiplierAbilityDef item5 = Repo.GetAllDefs<DamageMultiplierAbilityDef>().FirstOrDefault((DamageMultiplierAbilityDef w) => w.name.Equals("AcidResistant_DamageMultiplierAbilityDef"));
+            HealthChangeStatusDef healthChangeStatusDef = Repo.GetAllDefs<HealthChangeStatusDef>().FirstOrDefault((HealthChangeStatusDef w) => w.name.Equals("E_Status [Mutog_Regeneration_AbilityDef]"));
+            tacticalActorDef.ShouldStopMovingOnActorSpotted = false;
+            ramAbilityDef.UsesPerTurn = usesPerTurn;
+            ramAbilityDef.ActionPointCost = actionPointCost;
+            ramAbilityDef.WillPointCost = (float)num;
+            ramAbilityDef.RamForce = 600;
+            ramAbilityDef.ViewElementDef.DisplayName1 = new LocalizedTextBind("FURIOUS CHARGE", false);
+            ramAbilityDef.ViewElementDef.Description = new LocalizedTextBind("Charge forward ramming everything in the way for the cost of AP with distance", false);
+            healthChangeStatusDef.HealthChangeAmount = (float)num2;
+            healthChangeStatusDef.TargetFirstDamagedBodypart = targetFirstDamagedBodypart;
+            applyStatusAbilityDef.ViewElementDef.Description = new LocalizedTextBind("Each leg restores 10 Hit Points to all injured body parts.", false);
+            List<AbilityDef> list = itemDef2.Abilities.ToList<AbilityDef>();
+            list.Add(item3);
+            list.Add(item2);
+            list.Add(item5);
+            list.Add(item4);
+            list.Add(item);
+            itemDef2.Abilities = list.ToArray();
         }
         public static void MainMod(Func<string, object, object> api)
         {
