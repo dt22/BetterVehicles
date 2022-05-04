94e3411ba3ccaf0f1d5df4064d5ebea797c8a945
dt22 <dtony22@gmail.com>
2022-05-03 22:26:00 -0500

    Added kill n run

---
+++ BetterVehicles/KillNRun.cs
@@ -0,0 +1,29 @@
+﻿using Base.Cameras.ExecutionNodes;
+using Base.Core;
+using Base.Defs;
+using Base.Entities.Abilities;
+using Base.Entities.Effects;
+using Base.Entities.Statuses;
+using Base.UI;
+using Harmony;
+using PhoenixPoint.Common.Core;
+using PhoenixPoint.Common.Entities;
+using PhoenixPoint.Common.Entities.GameTags;
+using PhoenixPoint.Common.Entities.GameTagsTypes;
+using PhoenixPoint.Common.UI;
+using PhoenixPoint.Tactical.Cameras.Filters;
+using PhoenixPoint.Tactical.Entities.Abilities;
+using PhoenixPoint.Tactical.Entities.Animations;
+using PhoenixPoint.Tactical.Entities.Statuses;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace BetterVehicles
+{
+    internal class KillNRun
+    {
+       
+    }
+}
